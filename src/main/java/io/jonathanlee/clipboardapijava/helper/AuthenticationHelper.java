57a235c39b665224a17473ba780450a227295789
Jonathan Lee <jonathan.lee.devel@gmail.com>
2023-01-10 19:49:51 +0000

    Minor change

--- src/main/java/io/jonathanlee/clipboardapijava/helper/AuthenticationHelper.java
+++ src/main/java/io/jonathanlee/clipboardapijava/helper/AuthenticationHelper.java
@@ -4,6 +4,10 @@ import org.springframework.security.core.context.SecurityContextHolder;
 
 public class AuthenticationHelper {
 
+  private AuthenticationHelper() {
+    // Private constructor to hide the implicit public one
+  }
+
   public static String getCurrentUsername() {
     return (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
   }
