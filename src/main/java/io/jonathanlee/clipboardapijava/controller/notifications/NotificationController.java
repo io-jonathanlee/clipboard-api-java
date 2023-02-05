6b344401cbb1f14d8a071bc32107dd96a5fbbd1f
Jonathan Lee <jonathan.lee.devel@gmail.com>
2023-02-05 21:08:48 +0000

    Minor name refactor

--- src/main/java/io/jonathanlee/clipboardapijava/controller/notifications/NotificationController.java
+++ src/main/java/io/jonathanlee/clipboardapijava/controller/notifications/NotificationController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/notifications")
-public class NotificationsController {
+public class NotificationController {
 
   private final NotificationService notificationService;
 
